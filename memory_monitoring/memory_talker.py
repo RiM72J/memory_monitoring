330b795f31dbe0336a54d953f805b465664aa3e8
RiMC7 <iinu04835@gmail.com>
2025-12-25 22:50:32 +0900

    新しくmemory_talkerを作り直し

--- memory_monitoring/memory_talker.py
+++ memory_monitoring/memory_talker.py
@@ -1,3 +1,11 @@
+import rclpy
+from rclpy.node import Node
+
+class MemoryTalker(Node):
+    def __init__(self):
+        super().__init__('memory_talker')
+        self.get_logger().info("Talker Node Created (Empty)")
+
 def main():
     rclpy.init()
     node = MemoryTalker()
