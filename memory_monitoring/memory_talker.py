e34b3aca0e7786c1ea061ead5f55c3349d652c33
RiMC7 <iinu04835@gmail.com>
2025-12-25 22:54:05 +0900

    add logic

--- memory_monitoring/memory_talker.py
+++ memory_monitoring/memory_talker.py
@@ -1,10 +1,24 @@
 import rclpy
 from rclpy.node import Node
+import psutil 
 
 class MemoryTalker(Node):
     def __init__(self):
         super().__init__('memory_talker')
-        self.get_logger().info("Talker Node Created (Empty)")
+        self.create_timer(1.0, self.cb) 
+
+    def cb(self):
+        procs = []
+        for p in psutil.process_iter(['name', 'memory_percent']):
+            try:
+                procs.append(p.info)
+            except:
+                pass
+        
+        sorted_procs = sorted(procs, key=lambda p: p['memory_percent'], reverse=True)
+        if sorted_procs:
+            top = sorted_procs[0]
+            self.get_logger().info(f"Debug: {top['name']} is using {top['memory_percent']}%")
 
 def main():
     rclpy.init()
