d57320321745eaac6714d2b648f8258ffec449fa
RiMC7 <iinu04835@gmail.com>
2025-12-27 17:01:10 +0900

    update memory_listener.py

--- memory_monitoring/memory_listener.py
+++ memory_monitoring/memory_listener.py
@@ -1,10 +1,15 @@
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String 
 
 class MemoryListener(Node):
     def __init__(self):
         super().__init__('memory_listener')
-        self.get_logger().info("Listener Node Created (Waiting for logic...)")
+        self.create_subscription(String, 'memory_usage', self.cb, 10)
+        self.get_logger().info("Listener started subscription")
+
+    def cb(self, msg):
+        self.get_logger().info(f"Received raw data: {msg.data}")
 
 def main():
     rclpy.init()
