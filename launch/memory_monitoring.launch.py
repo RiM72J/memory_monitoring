0abfbff456117d21e9404e3097fc829733819ef3
RiMC7 <iinu04835@gmail.com>
2025-12-27 22:02:51 +0900

    add launch file

---
+++ launch/memory_monitoring.launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='memory_monitoring',
+            executable='talker',
+            name='memory_talker'
+        ),
+        Node(
+            package='memory_monitoring',
+            executable='listener',
+            name='memory_listener'
+        )
+    ])
