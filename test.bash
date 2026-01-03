411736c2e496f763cbc7cfd005c85c9d7912b732
RiMC7 <iinu04835@gmail.com>
2026-01-03 20:33:26 +0900

    update2 test.bash

--- test.bash
+++ test.bash
@@ -1,15 +1,26 @@
 #!/bin/bash -xv
-# SPDX-FileCopyrightText: 2025 RiMC7
+# SPDX-FileCopyrightText: 2025 Ryomu Inukai
 # SPDX-License-Identifier: BSD-3-Clause
 
-dir=~
-[ "$1" != "" ] && dir="$1"
+ng () {
+      echo ${1}行目が違うよ
+      res=1
+}
 
-cd $dir/ros2_ws
+res=0
 
-colcon build --packages-select memory_monitoring
-source install/setup.bash
+source /opt/ros/jazzy/setup.bash
+cd $HOME/ros2_ws
 
-timeout 15 ros2 launch memory_monitoring memory_monitoring.launch.py > /tmp/memory_monitoring.log
+colcon build --packages-select memory_monitoring || ng "$LINENO"
+source $HOME/ros2_ws/install/setup.bash
 
-cat /tmp/memory_monitoring.log | grep 'Normal:'
+timeout 15 ros2 launch memory_monitoring memory_monitoring.launch.py > /tmp/memory_monitoring.log 2>&1 || true
+
+count=$(grep -c "Normal:" /tmp/memory_monitoring.log)
+
+[ "$count" -ge 1 ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
